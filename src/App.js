e821daf34b15f751faf863f03fc9fb871ef1cdb5
gsllucas <llucasgs@gmail.com>
2022-05-03 00:33:15 -0300

    :boom: Starting react studies project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const client01 = {
+  client: "Luana",
+  age: 27,
+  purchases: [
+    { product: "Notebook", price: "R$2500" },
+    { product: "Geladeira", price: "R$3000" },
+    { product: "Smartphone", price: "R$1500" },
+  ],
+  isActive: true,
+};
+
+const client02 = {
+  client: "Mario",
+  age: 31,
+  purchases: [
+    { product: "Notebook", price: "R$5500" },
+    { product: "Geladeira", price: "R$5000" },
+    { product: "Smartphone", price: "R$4500" },
+  ],
+  isActive: false,
+};
+
+function calculateTotalExpense(purchases) {
+  return purchases.reduce((acc, purchase) => {
+    const formattedPrice = Number(purchase.price.replace("R$", ""));
+    return (acc += formattedPrice);
+  }, 0);
 }
+
+const App = () => {
+  const dados = client02;
+  const { client, age, purchases, isActive } = dados;
+  const totalExpend = calculateTotalExpense(purchases);
+
+  return (
+    <React.Fragment>
+      <div>
+        <p>Name: {client}</p>
+        <p>Age: {age}</p>
+        <p>
+          Situation:{" "}
+          <span style={{ color: isActive ? "green" : "red" }}>
+            {isActive ? "Active" : "Inactive"}
+          </span>
+        </p>
+        <p>Total Expend: {totalExpend}</p>
+        {totalExpend > 10000 && <p>You're expending too much.</p>}
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default App;
